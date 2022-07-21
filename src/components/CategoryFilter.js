514c79bb7989cf757660984384f1d18699654aa6
winston kiprop <winstonekip50@gmail.com>
2022-07-22 01:24:04 +0300

    Done.

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onSelectCategory}) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map(category=>{
+        return <button 
+                  key={category} 
+                  className= {category === selectedCategory ? "selected" : null}
+                  onClick={()=>onSelectCategory(category)}
+               >
+                 {category}
+               </button>
+      })}
     </div>
   );
-}
+}}
 
 export default CategoryFilter;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,25 @@
+// import React from "react";
+
+// function TaskList() {
+//   return (
+//     <div className="tasks">
+//       {/* display a list of tasks using Task component */}
+//     </div>
+//   );
+// }
+
+// export default TaskList;
 import React from "react";
+import Task from "./Task"
 
 function TaskList() {
+function TaskList({tasks, onDeleteTask}) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map(task=><Task key={task.text} text={task.text} category={task.category} onDeleteTask={onDeleteTask}/>)}
     </div>
   );
-}
+}}
 
 export default TaskList;
